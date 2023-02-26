eb8c62770bc8f927e27599072316e5f49eff6f85
RuiWang98 <benjaminwr98@gmail.com>
2023-02-26 10:44:09 -0500

    Updated setting page and sync to master version

--- src/screens/DoctorProfileScreen.tsx
+++ src/screens/DoctorProfileScreen.tsx
@@ -1,6 +1,6 @@
-import { Box, Button, makeStyles, Stack, TextField, Typography } from "@mui/material";
+import { Box, Button, Container, makeStyles, Stack, TextField, Typography } from "@mui/material";
 import * as React from "react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 type DoctorProfile = {
   name: string;
@@ -38,73 +38,82 @@ export default function DoctorProfileScreen() {
     }));
   };
 
-  const handleSave = () => {
+  const handleSave = (e: FormEvent) => {
+    e.preventDefault()
     // TODO: Save profile
   };
 
   return (
-    <>
-      <Typography variant="h5" gutterBottom>
-        {/* eslint-disable-next-line react/no-unescaped-entities */}
-        Doctor {editableProfile.name}'s Profile
-      </Typography>
-      <form noValidate autoComplete="off">
-        <Stack direction="column" spacing={3} pt={3}>
-          <TextField
-            fullWidth
-            variant="filled"
-            label="Email"
-            name="email"
-            value={editableProfile.email}
-          />
-          <TextField
-            fullWidth
-            label="Name"
-            name="name"
-            value={editableProfile.name}
-            onChange={handleChange}
-          />
-          <TextField
-            fullWidth
-            label="Date Of Birth"
-            name="dateOfBirth"
-            value={editableProfile.dateOfBirth}
-            onChange={handleChange}
-          />
-          <TextField
-            fullWidth
-            label="Phone"
-            name="phone"
-            value={editableProfile.phone}
-            onChange={handleChange}
-          />
-          <TextField
-            fullWidth
-            label="Address"
-            name="address"
-            value={editableProfile.address}
-            onChange={handleChange}
-          />
-          <TextField
-            fullWidth
-            label="Specialty"
-            name="specialty"
-            value={editableProfile.specialty}
-            onChange={handleChange}
-          />
-          <TextField
-            fullWidth
-            label="Doctor Registration Number"
-            name="doctorRegistrationNumber"
-            value={editableProfile.doctorRegistrationNumber}
-            onChange={handleChange}
-          />
+    <Box>
+      <Container>
+        <>
+          <Typography variant="h5" gutterBottom>
+          </Typography>
+          <form noValidate autoComplete="off" onSubmit={handleSave}>
+            <Stack spacing={5} padding={5}>
+              <Typography variant="h4">Profile</Typography>
+              <TextField
+                fullWidth
+                variant="filled"
+                label="Email"
+                name="email"
+                value={editableProfile.email}
+              />
+              <TextField
+                fullWidth
+                label="Name"
+                name="name"
+                value={editableProfile.name}
+                onChange={handleChange}
+              />
+              <TextField
+                fullWidth
+                label="Date Of Birth"
+                name="dateOfBirth"
+                value={editableProfile.dateOfBirth}
+                onChange={handleChange}
+              />
+              <TextField
+                fullWidth
+                label="Phone"
+                name="phone"
+                value={editableProfile.phone}
+                onChange={handleChange}
+              />
+              <TextField
+                fullWidth
+                label="Address"
+                name="address"
+                value={editableProfile.address}
+                onChange={handleChange}
+              />
+              <TextField
+                fullWidth
+                label="Specialty"
+                name="specialty"
+                value={editableProfile.specialty}
+                onChange={handleChange}
+              />
+              <TextField
+                fullWidth
+                label="Doctor Registration Number"
+                name="doctorRegistrationNumber"
+                value={editableProfile.doctorRegistrationNumber}
+                onChange={handleChange}
+              />
+              <Stack direction="row" spacing={5} sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Button variant="contained" color="secondary" onClick={() => { window.location.reload() }}>
+                  Discard
+                </Button>
+                <Button variant="contained" color="primary" type="submit" onClick={handleSave}>
+                  Submit
+                </Button>
+              </Stack>
+            </Stack>
+          </form>
+        </>
+      </Container>
+    </Box>
 
-          <Button variant="contained" onClick={handleSave}>
-            Save
-          </Button>
-        </Stack>
-      </form>
-    </>
   );
 }
