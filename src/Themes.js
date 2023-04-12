8133bedf73fe0852e6d26c1f0a4612bd16d67956
sheen404 <harsheenkaur99@gmail.com>
2023-04-12 17:19:09 -0400

    Doctor Alignment Correction

--- src/Themes.js
+++ src/Themes.js
@@ -1,76 +1,73 @@
-import React from 'react';
-import { createTheme, ThemeProvider, colors} from '@mui/material';
+import React from "react";
+import { createTheme, ThemeProvider, colors } from "@mui/material";
 
 export const patientTheme = createTheme({
-	palette:{
-		primary:{
-			dark: '#DCE7D7',
-			main: '#A9C39E',
-			contrastText: '#153D3C'
-		},
-		secondary:{
-			main:'#F6B56B',
-			dark:'#F1876F',
-			
-		}
-	}
-})
+  palette: {
+    primary: {
+      dark: "#DCE7D7",
+      main: "#A9C39E",
+      contrastText: "#153D3C",
+    },
+    secondary: {
+      main: "#F6B56B",
+      dark: "#F1876F",
+    },
+  },
+});
 
 export const counselorTheme = createTheme({
-	palette:{
-		primary:{
-			main: '#BADFE7',
-			dark: '#C8F8EA',
-			contrastText: '#0C293E',
-			light:'#84EFD0'
-		},
-		secondary:{
-			main:'#3B5365',
-			dark:'#F1876F',
-			contrastText: '#FFFFFF',
-		}
-	}
-})
+  palette: {
+    primary: {
+      main: "#BADFE7",
+      dark: "#C8F8EA",
+      contrastText: "#0C293E",
+      light: "#84EFD0",
+    },
+    secondary: {
+      main: "#3B5365",
+      dark: "#F1876F",
+      contrastText: "#FFFFFF",
+    },
+  },
+});
 
 export const doctorTheme = createTheme({
-	palette:{
-		primary:{
-			main: '#6FB3B8',
-			light: '#D9D9FF'
-		},
-		secondary:{
-			main:'#FEFF89',
-			dark:'#F1876F'
-		}
-	}
-})
+  palette: {
+    primary: {
+      main: "#6FB3B8",
+      light: "#D9D9FF",
+    },
+    secondary: {
+      main: "#FEFF89",
+      dark: "#F1876F",
+      light: "#FFFFFF",
+    },
+  },
+});
 
 export const managerTheme = createTheme({
-	palette:{
-		primary:{
-			main: '#388087',
-			contrastText: '#FFFFFF'
-		},
-		secondary:{
-			main:'#FEFF89',
-			contrastText: '#000000'
-		}
-		
-	}
-})
-
+  palette: {
+    primary: {
+      main: "#388087",
+      contrastText: "#FFFFFF",
+    },
+    secondary: {
+      main: "#FEFF89",
+      contrastText: "#000000",
+    },
+  },
+});
 
 export const baseTheme = createTheme({
-	palette:{
-		primary:{
-			main: '#6B6891',
-			contrastText: '#FFFFFF',
-			dark: '#313131'
-			
-		},
-		secondary:{
-			main:'#FFFFFF',
-			dark: '#CB947A'
-		}
-	}
-})
+  palette: {
+    primary: {
+      main: "#6B6891",
+      contrastText: "#FFFFFF",
+      dark: "#313131",
+    },
+    secondary: {
+      main: "#FFFFFF",
+      dark: "#CB947A",
+    },
+  },
+});
